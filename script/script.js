680ee91a8d0d629dfb001a980efaa705f994fd07
Nikolay Angelov <n.angelov@yahoo.com>
2016-06-21 10:21:34 +0200

    Added Files
    first commit

---
+++ script/script.js
@@ -0,0 +1,108 @@
+
+
+/*
+$(document).ready(function() {
+
+    console.log("HELLO JS");
+
+function validateXXX(){
+    console.log("Real validation");
+
+    $('#ctr_name, #ctr_email, #ctr_message').bind('keyup', function() {
+        if(allFilled()) $('#contract_btn').removeAttr('disabled');
+    });
+
+      function allFilled() {
+          var filled = true;
+          $('form.validator').each(function() {
+              console.log("loops:" + $(this).val());
+              if($(this).val() == '') filled = false;
+          });
+          return filled;
+        }
+}
+
+});
+
+*/
+
+$(document).ready(function(){
+
+  console.log("Working , not?");
+
+  var instaFeed = new Instafeed({
+              get: 'user',
+              userId: 2318655595,
+              accessToken: '2318655595.a0816c8.22d595779a9442ee92ad9dea1e3b1ea0',
+              target: 'instafeed',
+              sortBy: 'most-liked',
+              limit: 100,
+              resolution: 'thumbnail'
+}); instaFeed.run();
+
+
+
+
+
+});
+
+
+
+
+function cnt_field_validation() {
+
+    $('#ctr_name, #ctr_email, #ctr_message').bind('keyup', function() {
+        if(allFilled()) $('#contract_btn').removeAttr('disabled');
+    });
+
+  console.log("are we working ? ");
+
+}
+
+
+function allFilled() {
+     var filled = true;
+          $('form.validator').each(function() {
+              console.log("loops:" + $(this).val());
+              if($(this).val() == '') filled = false;
+          });
+      return filled;
+}
+
+
+
+  function validateForm() {
+
+    var x = document.forms["contactForm"]["ct_name"]["ct_email"]["ct_message"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        console.log("validate?");
+        return false;
+    }
+}
+
+
+  function validate()
+      {
+
+         if( document.contactForm.ct_name.value == "" )
+         {
+            alert( "Моля, попълнете Вашето име!" );
+            document.contactForm.ct_name.focus() ;
+            return false;
+         }
+
+         if( document.contactForm.ct_email.value == "" )
+         {
+            alert( "Моля, попълнете Вашият e-mail!" );
+            document.contactForm.ct_email.focus() ;
+            return false;
+         }
+
+         if( document.contactForm.ct_message.value == "" )
+         {
+            alert( "Моля, напишете съобщение!" );
+            document.contactForm.ct_message.focus() ;
+            return false;
+         }
+      }
